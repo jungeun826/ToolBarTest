20745834dcb0441bb53a71280a347db71c627910
jungeun826 <jungeun826@nate.com>
2014-01-03 14:49:16 +0900

    ToolBarTest 완료
    아이폰 실습 2.9.1 툴바 연습완료했다.

--- ToolBarTest/ViewController.m
+++ ToolBarTest/ViewController.m
@@ -13,12 +13,48 @@
 @end
 
 @implementation ViewController
+- (IBAction)handleBarButtonClick:(id)sender {
+    UIBarButtonItem *button = (UIBarButtonItem *)sender;
+    
+    if( button.tag == 1){
+        NSLog(@"\nLeft 버튼");
+    }else{
+        NSLog(@"\nRight 버튼");
+    }
+}
+
+-(void)handleRefresh:(id)sender {
+    NSLog(@"Refresh!!");
+}
+
+-(void)handleButton:(id)sender {
+    NSLog(@"Button Click!!");
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 160, 320, 60)];
+    
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc]initWithTitle:@"Button" style:UIBarButtonItemStyleDone target:self action:@selector(handleButton:)];
+    
+    NSArray *segments = [NSArray arrayWithObjects:@"A",@"B",@"C", nil];
+    
+    UISegmentedControl *segment = [[UISegmentedControl alloc] initWithItems:segments];
+    segment.frame  = CGRectMake(0, 0, 140, 44);
+    UIBarButtonItem *segmentButton = [[UIBarButtonItem alloc] initWithCustomView:segment];
+    
+    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    //space.width = 70;
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(handleRefresh:)];
+    
+    NSArray *items = [NSArray arrayWithObjects:titleButton,segmentButton, space, refreshButton, nil];
+    [toolBar setItems:items];
+    [self.view addSubview:toolBar];
+
 }
+
 
 - (void)didReceiveMemoryWarning
 {
